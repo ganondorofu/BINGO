74a391e906cb8e838fcba38389a2cd482ec25cb0
ganondorofu <167788682+ganondorofu@users.noreply.github.com>
2024-04-30 23:59:25 +0900

    Add files via upload

---
+++ BINGO/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace BINGO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 20F);
+            this.button1.Location = new System.Drawing.Point(45, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 79);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 80);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BINGO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(100, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 67);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(85, 293);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 80);
+            this.label3.TabIndex = 3;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(271, 189);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 172);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(266, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "出た数字一覧";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(58, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 80);
+            this.label5.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(326, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 67);
+            this.label6.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 393);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "INGO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ BINGO/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BINGO
+{
+
+    public partial class Form1 : Form
+    {
+        private List<int> list; //listをクラスのメンバー変数に変更
+        private List<int> result;
+        private int rand;
+        string Text = ""; // 初期化する
+        private int num = 0;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            list = new List<int>(); //コンストラクター内で初期化
+            result = new List<int>();
+            
+            for (int i = 1; i <= 75; i++) //リストに1-75まで追加する。そこから一つずつ消していき被らないようにする。
+            {
+                list.Add(i);
+            }
+            System.Random r = new System.Random();
+            rand = r.Next(0, list.Count); // 乱数の範囲を修正
+            num = list[rand];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Random r = new System.Random();
+            if (list.Count > 0) // リストが空でないことを確認
+            {
+                if (1 <= num && num <= 15) { Text = "B"; } // アルファベットの振り分け
+                else if (15 < num && num <= 30) { Text = "I"; } // else if を使用し、条件の重複を回避
+                else if (30 < num && num <= 45) { Text = "N"; }
+                else if (45 < num && num <= 60) { Text = "G"; }
+                else if (60 < num && num <= 75) { Text = "O"; }
+                list.Remove(num); // 最初のリストから削除
+                result.Add(num); // あとから見返せるように出た数字を記録するリスト
+                result.Sort();
+                listBox1.Items.Clear(); // リストを数字順にするため一度リストを初期化する
+                foreach (int item in result) // リストに一括追加
+                {
+                    listBox1.Items.Add(item);
+                }
+                label3.Text = num.ToString();
+                label2.Text = Text;
+                rand = r.Next(0, list.Count); // 乱数の範囲を修正
+                num = list[rand];
+                //label6.Text = num.ToString();
+                Console.WriteLine(string.Join(", ", list));
+                if (list.Count == 0) // 最初のリストが空になった時
+                {
+                    label5.Text = ("End"); // ビンゴが終わったことを表示する
+                    label3.Text = ("");
+                    label2.Text = ("");
+                }
+            }
+        }
+    }
+}
